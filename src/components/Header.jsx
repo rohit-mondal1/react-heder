48aee8abfda1bbb023d9f156cce1cc5214eb3c9f
rohit-mondal1 <antardwip.mtv@gmail.com>
2024-02-08 15:13:46 +0530

    upd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,20 @@ import React, { useState } from 'react';
 import { BookOpenIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
-    let Links =[
+    const Links =[
         {name:"HOME",link:"/"},
         {name:"SERVICE",link:"/"},
         {name:"ABOUT",link:"/"},
         {name:"CONTACT",link:"/"},
       ];
-      let [open, setOpen] =useState(false);
+      const [open, setOpen] =useState(false);
 
     return (
         <div className='shadow-md w-full fixed top-0 left-0'>
            <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
             {/* logo section */}
             <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-                <BookOpenIcon className='w-7 h-7 text-blue-600'/>
-                <span>Inscribe</span>
+                <span>icon</span>
             </div>
             {/* Menu icon */}
             <div onClick={()=>setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
